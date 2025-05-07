7fb9908548eabb821fab5ee6b0f5a78149a37d61
helmihakimudin <helmihakimudin@gmail.com>
2025-05-07 15:33:49 +0700

    initial commit

---
+++ src/states/isPreload/action.js
@@ -0,0 +1,47 @@
+/**
+ * @TODO: Define all the actions (creator) for the isPreLoad state
+ */
+
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+function setIsPreloadActionCreator(isPreload) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreload,
+    },
+  };
+}
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      // preload process
+      dispatch(showLoading());
+
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      // fallback process
+      console.log(error);
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      // end preload process
+      dispatch(setIsPreloadActionCreator(false));
+    }
+    dispatch(hideLoading());
+
+  };
+}
+
+export {
+  ActionType,
+  setIsPreloadActionCreator,
+  asyncPreloadProcess,
+};
